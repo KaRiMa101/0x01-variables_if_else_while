fc8606ab61d2a39998446a7433377325551fcab6
KaRiMa101 <karima.baddag10@gmail.com>
2023-03-18 08:40:44 -0700

    0-positive_or_negative commit -8

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
- *this is main function
  *main - Determine if a the number is positive or negative
+ *
  *Return Always 0 (Success)
  */
 int main(void)
