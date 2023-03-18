81baf86072a8b4df8d8e694b799157abd94d385c
KaRiMa101 <karima.baddag10@gmail.com>
2023-03-18 08:18:34 -0700

    0-positive_or_negative commit -3

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,17 +1,16 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
-int main(void)
 /*this is main function*/
+int main(void)
 {   int n;
     srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n == 0) 
+	if (n == 0)
 	printf ("%d is zero\n", n);
-	else  if (n < 0) 
+	else if (n < 0)
 	    printf ("%d is negative\n", n);
-
-	else  printf ("%d is positive\n", n);
+	else  
+	printf ("%d is positive\n", n);
 		return (0);
 }
