9b4f506b1c377f18e4a8132b70e62888cbc87211
KaRiMa101 <karima.baddag10@gmail.com>
2023-03-18 11:44:02 -0700

    prit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - this is main program
+ * Description:  print numbers 0 to 9
+ * followed by a new line
+ * Return: Always 0
+ */
+int main(void)
+{
+int l;
+for (l = 0; l < 10 ; l++)
+{
+printf("%d", l);
+}
+printf('\n');
+return (0);
+}
