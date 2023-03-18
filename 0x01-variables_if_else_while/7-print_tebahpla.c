bc4f0a74688b61923330f14547b36f6728eadcdd
KaRiMa101 <karima.baddag10@gmail.com>
2023-03-18 12:14:21 -0700

    alphabets in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - this is main program
+ * Description:  the alphabet in reverse
+ * followed by a new line
+ * Return: Always 0
+ */
+int main(void)
+{
+char letters[26] = "abcdefghijklmnopqrstuvwxyz";
+int l;
+for (l = 26; l > 0 ; l--)
+{
+putchar(letters[l]);
+}
+putchar('\n');
+return (0);
+}
