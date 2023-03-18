fd1848bb7eeafb9fbd501e0b8a59bd4b5be32773
KaRiMa101 <karima.baddag10@gmail.com>
2023-03-18 11:58:58 -0700

    prit numbers-1

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,17 +4,16 @@
 
 /**
  * main - this is main program
- * Description: prints all single digit numbers 0 to 10
+ * Description: prints all single digit numbers 0 to 9
  * followed by a new line
  * Return: Always 0
  */
 int main(void)
 {
-char numbers [10] = "0123456789";
-int l;
-for (l = 0; l < 10 ; l++)
+int n ;
+for (n = 0; n < 10 ; n++)
 {
-putchar(numbers[l]);
+putchar(n);
 }
 putchar('\n');
 return (0);
