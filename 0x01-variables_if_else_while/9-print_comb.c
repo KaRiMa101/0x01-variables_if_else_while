d9f5903ca899ca60bdc74504d10698929eae497e
KaRiMa101 <karima.baddag10@gmail.com>
2023-03-18 12:32:29 -0700

    prit with separator

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - this is main program
+ * Description:  the numbers seperated by caracter
+ * followed by a new line
+ * Return: Always 0
+ */
+int main(void)
+{
+int l,k ;
+for (l = '0'; l <= '9' ; l++)
+{
+putchar(l);
+k=l;
+if (k <= '8')
+putchar(" ,");
+}
+putchar('\n');
+return (0);
+}
