0fb8df89a9498c8798522a2448544219bec53aec
KaRiMa101 <karima.baddag10@gmail.com>
2023-03-18 11:38:16 -0700

    alphabet except e and q -1

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -14,8 +14,7 @@ char letters[26] = "abcdefghijklmnopqrstuvwxyz";
 int l;
 for (l = 0; l < 26 ; l++)
 {if(letters[l] != 'e' && letters[l] != 'q')
-    putchar(letters[l]);
-    
+putchar(letters[l]);
 }
 putchar('\n');
 return (0);
