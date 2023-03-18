dd011fbda4716cf6be9d7809153a528cae697b13
KaRiMa101 <karima.baddag10@gmail.com>
2023-03-18 11:37:14 -0700

    alphabet except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - this is main program
+ * Description:  the alphabet except e and q
+ * followed by a new line
+ * Return: Always 0
+ */
+int main(void)
+{
+char letters[26] = "abcdefghijklmnopqrstuvwxyz";
+int l;
+for (l = 0; l < 26 ; l++)
+{if(letters[l] != 'e' && letters[l] != 'q')
+    putchar(letters[l]);
+    
+}
+putchar('\n');
+return (0);
+}
