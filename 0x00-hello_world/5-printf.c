c6abfd7152a2a2b9e75d4e2a2860ab08f63e007d
KaRiMa101 <karima.baddag10@gmail.com>
2023-03-19 15:28:08 -0700

    the 5th task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is main program
+ * Description:  prints a line
+ * followed by a new line
+ * Return: Always 0
+ */
+int main(void)
+{
+puts("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
