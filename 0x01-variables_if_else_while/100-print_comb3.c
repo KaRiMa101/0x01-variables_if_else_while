81e179b2ab387458b17d7481004749b1a5ca856d
KaRiMa101 <karima.baddag10@gmail.com>
2023-03-19 13:51:01 -0700

    printnumb combo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - this is main program
+ * Description:  print combo of two numbers
+ * followed by a new line
+ * Return: Always 0
+ */
+int main(void)
+{
+int l, k;
+for (l = '0'; l <= '8' ; l++)
+{
+for (k = '1'; k <= '9' ; k++)
+{if (l<k)
+{
+putchar(l);
+putchar(k);
+putchar(',');
+putchar(' ');
+}
+}}
+
+return (0);
+}
