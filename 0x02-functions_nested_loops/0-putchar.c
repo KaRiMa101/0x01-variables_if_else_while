5fb5b9f6453c6471a5f2b9794015ad81ca0f3b8a
KaRiMa101 <karima.baddag10@gmail.com>
2023-03-22 16:18:14 -0700

    commit 1st task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,20 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - this is main program
  * Description: prints an expression
  * Return: Always 0
  */
-int main()
+int main(void)
 {
-putchar("_putchar\n");
-return 0;
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
+
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - this is main program
+ * Description: prints an expression
+ * Return: Always 0
+ */
+#include <unistd.h>
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
